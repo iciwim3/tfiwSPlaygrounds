342ee97e27cff17466129309778bc92344d3dd34
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-12-09 20:47:29 -0800

    3. Showed the power of protocols even more by creating a quiz template.

--- Protocols/protocol-jamming.playground/Contents.swift
+++ Protocols/protocol-jamming.playground/Contents.swift
@@ -59,18 +59,54 @@ let x: Double = 35.35342
 let y: Int = 8
 let q = x / y
 
+class Question {
+    var type: QuestionType
+    var query: String
+    var answer: String
+    
+    init(type: QuestionType, query: String, answer: String) {
+        self.type = type
+        self.query = query
+        self.answer = answer
+    }
+}
 
+enum QuestionType: String {
+    case trueFalse = "Am I legend?"
+    case multipleChoice = "Who is the greatest rapper of all time: Biggie, Tupac, JAY Z, Nas, or Andre 3000?"
+    case shortAnswer = "What is the capital of Texas?"
+    case essay = "In 50 words describe protocols."
+    
+    // Static constants are created for the lifecycle of the app.
+    static let types = [trueFalse, multipleChoice, shortAnswer, essay]
+}
 
+enum AnswerType: String {
+    case trueFalse = "True"
+    case multipleChoice = "Chamilionaire"
+    case shortAnswer = "Austin"
+    case essay = "Protocols in Swift provide a blueprint for functionality. They do not actually do anything in of themselves until implemented."
+    
+    static let types = [trueFalse, multipleChoice, shortAnswer, essay]
+}
 
+protocol QuestionGenerator {
+    func generateRandomQuestion() -> Question
+}
 
+class Quiz: QuestionGenerator {
+    func generateRandomQuestion() -> Question {
+        let randomNumb = Int(arc4random_uniform(4))
+        let randomType = QuestionType.types[randomNumb]
+        let randomQuery = randomType.rawValue
+        let randomAnswer = AnswerType.types[randomNumb].rawValue
+        let randomQuestion = Question(type: randomType, query: randomQuery, answer: randomAnswer)
+        return randomQuestion
+    }
+}
 
+let quiz = Quiz()
 
+let firstQuestion = quiz.generateRandomQuestion()
 
-
-
-
-
-
-
-
-
+print("QUESTION TYPE: \(firstQuestion.type) \nQUERY: \(firstQuestion.query) \nANSWER: \(firstQuestion.answer)")
