dd39ab4206f3d515b8de72a69f306f91bad9ce19
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-12-10 19:38:32 -0800

    2. Committing the enum SwitchStatus to the remote file.

---
+++ Protocols/The Light - The Dark/The Light - The Dark/Enums/SwitchStatus.swift
@@ -0,0 +1,22 @@
+//
+//  SwitchStatus.swift
+//  The Light - The Dark
+//
+//  Created by Sain-R Edwards Jr. on 12/10/17.
+//  Copyright © 2017 Appybuildmore Apps. All rights reserved.
+//
+
+import UIKit
+
+enum SwitchStatus: Toggleable {
+    case on, off
+    
+    mutating func toggle() {
+        switch self {
+        case .on:
+            self = .off
+        case .off:
+            self = .on
+        }
+    }
+}
